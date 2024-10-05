8fb19e2b547431fea7ba5738168e2ad9c6ac5c4b
cass cochrane <150483589+cass0023@users.noreply.github.com>
2024-10-05 16:45:24 -0400

    Add collect mechanic and game manager
    -added sprite changing direction with playercontroller
    -added game manager to count what collectibles the player has interacted with
    -added item collect mechanic that checks when gameobj that is attached to player collides with a 'collectible' prefab and deletes obj
     - this will call game manager function that will update counter/ui

---
+++ Devcon 2/Assets/Scripts/ItemCollect.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollect : MonoBehaviour
+{
+    public GameManager gameManager;
+    public PlayerController playerController;
+    
+    //obj attached to player to act as 'net'
+    public GameObject collect;
+    
+    public void OnTriggerStay2D(Collider2D collider){
+        //if the player clicks on this gameobj it will add to the counter in game manager
+        //and will destroy the obj -cc
+        //**need to add stuff to gamemanager functions to count items collected
+
+        if(playerController.interact == true && collider.gameObject == collect){
+            CheckObject();
+            Destroy(gameObject);       
+        }
+
+    }
+    private void CheckObject(){
+        if (gameObject.CompareTag("Rat")){
+            gameManager.RatCounter();
+            Debug.Log("Interacted with rat");
+        }
+        if (gameObject.CompareTag("Frog")){
+            gameManager.FrogCounter();
+            Debug.Log("Interacted with frog");
+        }
+        if (gameObject.CompareTag("Butterfly")){
+            gameManager.ButterflyCounter();
+            Debug.Log("Interacted with butterfly");
+        } 
+    }
+}
--- Devcon 2/Assets/Scripts/PlayerController.cs
+++ Devcon 2/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public Transform player;
     public KeyCode up;
     public KeyCode down;
     public KeyCode left;
     public KeyCode right;
     public float moveSpeed = 10f;
-    public bool interact;
+    public bool interact = false;
+    public bool facingRight = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -35,14 +35,31 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(right))
         {
             transform.Translate(new Vector2(1, 0) * moveSpeed * Time.deltaTime);
+            if(!facingRight){
+                ChangeDirection();
+            }
         }
         if (Input.GetKey(left))
         {
             transform.Translate(new Vector2(-1, 0) * moveSpeed * Time.deltaTime);
+            if(facingRight){
+                ChangeDirection();
+            }
         }
+        //checks if player is pressing left mouse button
         if(Input.GetKeyDown(KeyCode.Mouse0)){
             interact = true;
-            Debug.Log("Player interact");
         }
+        if(Input.GetKeyUp(KeyCode.Mouse0)){
+            interact = false;
+        }
+    }
+    public void ChangeDirection(){
+        //changes the players x scale to -1 so the sprite flips when changing direction -cc
+        //www.youtube.com/watch?v=Cr-j7EoM8bg
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
     }
 }
