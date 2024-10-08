add5393d5e0c76393bb487811fcaf3b3281e3ebc
Brianna Chaleunsouk <brichal2003@gmail.com>
2024-10-08 14:06:37 -0400

    Re Add UI

--- Devcon 2/Assets/Scripts/GameManager.cs
+++ Devcon 2/Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
+    //UI TextMeshPros
+    public TextMeshProUGUI butterflyCounter;
+    public TextMeshProUGUI frogCounter;
+    public TextMeshProUGUI cauldronCounter;
+
     // Start is called before the first frame update
     void Start()
     {
